08d53cafcd63dfb95661ed9a88d0a5fdca63d790
Sreevishnu07 <152363871+Sreevishnu07@users.noreply.github.com>
2026-04-02 17:23:05 +0530

    updated main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from PIL import Image
+import io
+
 from app.model.model import load_model, preprocess_image, predict
 
 app = FastAPI()
 
 model = load_model()
 
+
 @app.get("/")
 def home():
     return {"message": "API running"}
 
-@app.get("/test-model")
-def test_model():
-    img = Image.new("RGB", (224, 224))  # dummy image
-    tensor = preprocess_image(img)
-    pred = predict(model, tensor)
-    return {"prediction": pred}
+
+@app.post("/predict")
+async def predict_image(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+
+        image = Image.open(io.BytesIO(contents)).convert("RGB")
+
+        tensor = preprocess_image(image)
+
+        result = predict(model, tensor)
+
+        return {
+            "filename": file.filename,
+            "prediction": result
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
