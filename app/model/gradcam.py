06a3ce0c5bedd1c4eb4565154fdcf5b7cd1b50c7
Sreevishnu07 <152363871+Sreevishnu07@users.noreply.github.com>
2026-04-06 18:46:15 +0530

    updated gradcam

--- app/model/gradcam.py
+++ app/model/gradcam.py
@@ -4,6 +4,11 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 
 
+def compute_focus_score(cam):
+    # cam values are between 0 and 1
+    return float(cam.max() - cam.mean())
+
+
 def generate_gradcam(model, input_tensor, image_np):
     target_layers = [model.layer4[-1]]
 
@@ -11,6 +16,8 @@ def generate_gradcam(model, input_tensor, image_np):
 
     grayscale_cam = cam(input_tensor=input_tensor)[0]
 
+    focus_score = compute_focus_score(grayscale_cam)
+
     visualization = show_cam_on_image(image_np, grayscale_cam, use_rgb=True)
 
-    return visualization
+    return visualization, focus_score
