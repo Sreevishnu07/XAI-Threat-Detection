0f5217b1db79755b5189d322a0853b607f758a6a
Sreevishnu07 <152363871+Sreevishnu07@users.noreply.github.com>
2026-04-06 10:33:37 +0530

    updated model.py

--- app/model/model.py
+++ app/model/model.py
@@ -5,7 +5,6 @@ from PIL import Image
 import json
 import urllib.request
 
-# Load ImageNet class labels once
 LABELS_URL = "https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json"
 class_idx = json.load(urllib.request.urlopen(LABELS_URL))
 
@@ -20,20 +19,19 @@ def preprocess_image(image: Image.Image):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize( 
+        transforms.Normalize(
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]
         )
     ])
-    image = transform(image).unsqueeze(0)
-    return image
+    return transform(image).unsqueeze(0)
 
 
 def predict(model, image_tensor):
     with torch.no_grad():
         output = model(image_tensor)
 
-        probs = torch.nn.functional.softmax(output, dim=1)  # 🔥 probabilities
+        probs = torch.nn.functional.softmax(output, dim=1)
         confidence, predicted = torch.max(probs, 1)
 
         label = class_idx[str(predicted.item())][1]
